2f8cae306025bc2ad671091909165b9e1fc8d5d0
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-05-12 15:13:18 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -10,24 +10,28 @@ git_pull () {
   git submodule update --init --recursive --remote
 }
 
-#if image not already here
-if [ -z "$(docker images -q $image:$tag 2> /dev/null)" ]; then
-  git_pull
-fi
-
-#cache to rebuild image evey week hard rebuild every month
-created_date="$(docker inspect -f '{{ .Created }}' $image:$tag)"
-created_week=$(date +'%V' -d +'%Y-%m-%dT%H:%M:%S' --date="$created_date")
-created_month=$(date +'%m' -d +'%Y-%m-%dT%H:%M:%S' --date="$created_date")
-current_week=$(date +'%V')
-current_month=$(date +'%m')
-if [ "$created_week" -ne "$current_week" ]; then
-  git_pull
-  [ "$created_month" -ne "$current_month" ] && cache='--no-cache'
-  log="$(docker build . -t $image:$tag $cache)"
+docker_build () {
+  log="$(docker build . -t $image:$tag $1)"
   if $? ; then
     echo "Error building image = $image:$tag" > /dev/stderr
     echo "$log" > /dev/stderr
+  fi
+}
+
+#if image not already here
+if [ -z "$(docker images -q $image:$tag 2> /dev/null)" ]; then
+  docker_build
+else
+  #cache to rebuild image evey week hard rebuild every month
+  created_date="$(docker inspect -f '{{ .Created }}' $image:$tag)"
+  created_week=$(date +'%V' -d +'%Y-%m-%dT%H:%M:%S' --date="$created_date")
+  created_month=$(date +'%m' -d +'%Y-%m-%dT%H:%M:%S' --date="$created_date")
+  current_week=$(date +'%V')
+  current_month=$(date +'%m')
+  if [ "$created_week" -ne "$current_week" ]; then
+    git_pull
+    [ "$created_month" -ne "$current_month" ] && cache='--no-cache'
+    docker_build $cache
   fi
 fi
 
