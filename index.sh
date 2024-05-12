c183ea663b2675a4c0395a3910ea0f4bd576c592
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-05-12 15:27:38 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -11,7 +11,7 @@ git_pull () {
 }
 
 docker_build () {
-  log="$(docker build . -t $image:$tag $1)" exit_code=$?
+  log="$(docker build . -t $image:$tag $1)"; exit_code=$?
   if $exit_code ; then
     echo "Error building image = $image:$tag" > /dev/stderr
     echo "$log" > /dev/stderr
