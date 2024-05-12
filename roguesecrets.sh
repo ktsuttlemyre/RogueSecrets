ff00e49efce9f61526cb9bfd6a5076154fb94ba0
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-05-11 21:40:47 -0400

    Update roguesecrets.sh

--- roguesecrets.sh
+++ roguesecrets.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+#custom image name
 image='rogueos/roguescrets'
 tag='latest'
-docker build . -t $image:$tag
+
+#cache to rebuild image evey week
+created_date="$(docker inspect -f '{{ .Created }}' $image:$tag)"
+created_week=$(date +'%V' -d +'%Y-%m-%dT%H:%M:%S' --date="$created_date")
+current_week=$(date +'%V')
+if [ "$created_week" -ne "$current_week" ]; then
+  log="$(docker build . -t $image:$tag --no-cache=true)"
+  if $? ; then
+    echo "Error building image = $image:$tag" > /dev/stderr
+    echo "$log" > /dev/stderr
+  fi
+fi
+
+#actual run parameters
 docker run \
   --interactive --tty --rm \
-  --volume "${PWD}:/tmp/roguesecrets" \
-  --workdir "/tmp/roguesecrets" \
-  $image:$tag roguesecrets.sh "$@"
+  --volume "${PWD}:/home/roguesecrets" \
+  --workdir "/home/roguesecrets" \
+  $image:$tag main.sh "$@"
