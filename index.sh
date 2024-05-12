e07f1e667c7818aa4f424f82888c3a71223fd7e1
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-05-12 19:41:26 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -37,7 +37,7 @@ else
 fi
 
 #Run image
-docker compose -f <( envsubst < docker-compose.yaml ) --env-file <( env ) run
+docker compose run -f <( envsubst < docker-compose.yaml ) --env-file <( env )
 # if ! [ -z "$is_service" ]; then
 #   docker compose -f <( envsubst < docker-compose.yaml ) down
 # fi
