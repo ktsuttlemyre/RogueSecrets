688cb423e5c9a9e90c87b5440d6a4e4795753b4b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-05-13 00:30:09 -0400

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-cd ..
-rm -rf RogueSecrets/
-docker rmi $(docker images --filter=reference="rogueos/*:*" -q) -f
-git clone https://github.com/ktsuttlemyre/RogueSecrets.git
-cd RogueSecrets/
-chmod +x ./index.sh ./reset.sh
-#./index.sh 
+
