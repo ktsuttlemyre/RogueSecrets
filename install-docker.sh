75a6562c212e5b8fc1c0e93ba55757744eaa42bb
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-07-13 20:03:23 -0400

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -66,7 +66,7 @@ echo \
 sudo apt-get update
 
 header 'installing docker'
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
 #Add the connected user "$USER" to the docker group. Change the user name to match your preferred user if you do not want to use your current user:
 sudo usermod -aG docker $USER
