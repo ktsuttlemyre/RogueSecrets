fc036f23777c5334cc4f620bd8f0ed84e8722130
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-07-30 17:59:10 -0400

    Update RogueArgs.sh

--- common/RogueArgs.sh
+++ common/RogueArgs.sh
@@ -78,7 +78,7 @@ while [[ $# -gt 0 ]]; do
       IFS='=' read -r key value <<< "${1:2}"; export ${key}="${value}"; shift ;;
     --*)
       #if value doesn't start with -
-      if [ -z "$2" ] | [[ ${2} == ^- ]]; then
+      if [ -z "$2" ] || [[ ${2} == ^- ]]; then
         export ${1:2}=true
         shift # past argument
       else
