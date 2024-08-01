eb44d4b11d99cd74e525b769888b53834889c8bd
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-08-01 16:50:40 -0400

    Update RogueArgs.sh

--- common/RogueArgs.sh
+++ common/RogueArgs.sh
@@ -16,7 +16,7 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 script_name=$(basename "$0")
 if ! ((return 0 2>/dev/null) && true); then
         echo "This script is expected to be sourced. Please use . or source commands to call $script_name from $parent_name"
-        exit 1
+        echo "exit 1"
 fi
 
 #prerequsite checks
