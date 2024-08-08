14b19223e3f3502a45d1410f104b59226a83b43a
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-08-08 16:53:53 -0400

    Update maptodrive.sh

--- function/maptodrive.sh
+++ function/maptodrive.sh
@@ -5,19 +5,28 @@
 create_destinations=false
 dry_run=false
 # Check if env file is provided
-[ -z "$1" ] && echo "Usage: $0 path_to_env_file" && exit 1
+config=${1:-../_hosts/steamdeck/RogueCLI/functions/src.json}
+[ -z "$config" ] && echo "Usage: $0 path_to_env_file" && exit 1
 
 # Source the env file
-[ ! -f "$1" ] && echo "Env file $1 not found." && exit 1
-set -o allexport
-source "$1"
-set +o allexport
+if [[ "$config" = *.env ]]; then
+	[ ! -f "$config" ] && echo "Env file $config not found." && exit 1
+	set -o allexport
+	source "$config"
+	set +o allexport
+elif [[ "$config" == *.json ]]; then
+	src_abs_path="$(jq -r '.src_abs_path' "$config")"
+	dest_abs_path="$(jq -r '.dest_abs_path' "$config")"
+else
+ 	echo "WHAT?!"
+  	exit 1
+fi
 
-# Source directory (you can change this to your desired directory)
-[ -z "$SOURCE_DIR" ] && echo "Variable SOURCE_DIR has no value" && exit 1
-
-# Check if the source directory exists
-[ ! -d "$SOURCE_DIR" ] && echo "Source directory does not exist: $SOURCE_DIR" && exit 1
+# Check required variables
+[ -z "$src_abs_path" ] && echo "Variable src_abs_path has no value" && exit 1
+[ ! -d "$src_abs_path" ] && echo "Source directory does not exist: $src_abs_path" && exit 1
+[ -z "$dest_abs_path" ] && echo "Variable dest_abs_path has no value" && exit 1
+[ ! -d "$dest_abs_path" ] && echo "Destination directory does not exist: $dest_abs_path" && exit 1
 
 # Function to check if a file is a symbolic link
 is_symlink() { [ -L "$1" ]; }
@@ -66,7 +75,7 @@ create_symlinks() {
         continue
      fi
      echo "creating softlink $src_file $dest_file"
-     ln -sf "$src_file" "$dest_file"
+     #ln -sf "$src_file" "$dest_file"
   done
 }
 
@@ -79,17 +88,35 @@ scrub_destination=false
 
 # Main loop
 # Iterate over the files in the source directory using a glob pattern
-for src_folder in "$SOURCE_DIR"/*; do
-  src_name=$(basename "$src_folder")
-  # Check if it's a file (not a directory)
-  [ ! -d "$src_folder" ] && echo "Not a directory $src_folder skipping" && continue
-  echo "src is $src_name"
-  echo "folder is ${!src_name}"
-  dest_folder="${!src_name}"
-  [ -z "$dest_folder" ] && echo "Source directory $src_folder does not map to a destination" && continue
+# for src_folder in "$SOURCE_DIR"/*; do
+#   src_name=$(basename "$src_folder")
+#   # Check if it's a file (not a directory)
+#   [ ! -d "$src_folder" ] && echo "Not a directory $src_folder skipping" && continue
+#   echo "src is $src_name"
+#   echo "folder is ${!src_name}"
+#   dest_folder="${!src_name}"
+#   [ -z "$dest_folder" ] && echo "Source directory $src_folder does not map to a destination" && continue
 
-  echo "Linking directory from $src_folder to $dest_folder"
-  create_symlinks "$src_folder" "$dest_folder"
+#   echo "Linking directory from $src_folder to $dest_folder"
+#   create_symlinks "$src_folder" "$dest_folder"
+# done
+
+
+jq -r 'to_entries[][]' | while read -r src_rpath ; do
+    read -r dest_rpath
+    echo "src_rpath=$src_rpath and value=$dest_rpath"
+    
+    src_folder="$src_abs_path/$src_rpath"
+    dest_folder="$dest_abs_path/$dest_rpath"
+
+    # Check if it's a file (not a directory)
+    [ -z "$src_folder" ] && echo "Source directory $src_folder does not map to a destination" && continue
+    [ ! -d "$src_folder" ] && echo "Not a directory $src_folder skipping" && continue
+    [ -z "$dest_folder" ] && echo "Source directory $dest_folder does not map to a destination" && continue
+    [ ! -d "$dest_folder" ] && echo "Not a directory $dest_folder skipping" && continue
+
+    echo "Linking directory from $src_folder to $dest_folder"
+    create_symlinks "$src_folder" "$dest_folder"
 done
 
 echo "done"
