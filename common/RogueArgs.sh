276e44e27cb6b888692f03832e5793a41b8a5162
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-08-13 19:31:16 -0400

    Update RogueArgs.sh

--- common/RogueArgs.sh
+++ common/RogueArgs.sh
@@ -44,6 +44,8 @@ if ! ( (return 0 2>/dev/null) && true || false); then
 fi
 
 #prerequsite checks
+version_tag=${version_tag:-}
+help=${help:-}
 [ -z "${version_tag}" ] && echo "Please add a version_tag variable to your $parent_name" && exit 0
 [ -z "${help}" ] && [ ! -f "$parent_dir/README.md" ] && echo "Please add a help variable to your script or a $parent_dir/README.md" && exit 0
 
