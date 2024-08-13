731f87d4a81d9f6dbb43b4e5443e5e523f69c66b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-08-13 18:58:32 -0400

    Update RogueArgs.sh

--- common/RogueArgs.sh
+++ common/RogueArgs.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #
-#
+set -uo pipefail
+( 
+script=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ ! -f "${script}log.sh" ]; then
+  curl -O https://raw.githubusercontent.com/Zordrak/bashlog/master/log.sh
+fi
+chmod +x "${script}log.sh"
+source "${script}log.sh"
+)
+
 RogueArgs=("$@")
 #simple debugger for this script only
 console () {
