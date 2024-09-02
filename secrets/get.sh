c2c6103dd80ab3564d4e5f3138a08780faced78d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-09-02 18:25:46 -0400

    Update get.sh

--- secrets/get.sh
+++ secrets/get.sh
@@ -133,7 +133,7 @@ while read -r name; do
 		# load as envirnment variable
 		log_info "exporting $name to shell environment"
 		export "$name=$data"
-  		echo "$name=$data" >> /host/ramdisk/.env
+  		echo "$name=$data" >> /host/session/.env
 	else
 		log_info "writing $name to $f_path"
 
