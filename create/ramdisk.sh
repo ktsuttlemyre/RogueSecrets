30d81298497a96b3a19e80d61064b3c537204c0b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-09-03 02:10:44 -0400

    Update ramdisk.sh

--- create/ramdisk.sh
+++ create/ramdisk.sh
@@ -32,6 +32,7 @@ case "$OSTYPE" in
   msys*)    echo "WINDOWS";exit 1 ;;
   cygwin*)  echo "ALSO WINDOWS";exit 1 ;;
   *)
+  	sudo mkdir -p /mnt/RogueCLI_ramdisk_25201
   	sudo mount -t tmpfs -o size=$size,mode=1777 tmpfs /mnt/$ramdisk
    	sudo systemctl daemon-reload
    	if [ ! -d /mnt/$ramdisk ]; then
